00eac8629e5f25384d28cadee71e09f02ef3f176
Felix Klauke <info@felix-klauke.de>
2019-05-29 12:27:22 +0200

    Fix typo.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker build --build-arg PAPERSPIGOT_CI_JOB=$PAPERSPIGOT_CI_JOB --build-arg PAPERSPIGOT_CI_BUILDNUMBER=$PAPERSPIGOT_CI_BUILDNUMBER -t felixklauke/paperspigot:$MINECRAFT_VERSION_TAG .
-docker push felixklauke/paperspigott:$MINECRAFT_VERSION_TAG
+docker push felixklauke/paperspigot:$MINECRAFT_VERSION_TAG
