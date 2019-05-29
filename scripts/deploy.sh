7592713829e28ac61b651730c95ef2412287ce31
Felix Klauke <info@felix-klauke.de>
2019-05-29 12:10:32 +0200

    Use correct environments in matrix and add correct build arguments.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@
 # Login to docker hub
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build --build-arg BUKKIT_VERSION=$MINECRAFT_VERSION_TAG -t felixklauke/paperspigot:$MINECRAFT_VERSION_TAG .
+docker build --build-arg PAPERSPIGOT_CI_JOB=$PAPERSPIGOT_CI_JOB --build-arg PAPERSPIGOT_CI_BUILDNUMBER=$PAPERSPIGOT_CI_BUILDNUMBER -t felixklauke/paperspigot:$MINECRAFT_VERSION_TAG .
 docker push felixklauke/paperspigott:$MINECRAFT_VERSION_TAG
