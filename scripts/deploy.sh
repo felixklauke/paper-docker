cafd72fae787327892cfdf92d12ca2b5948fb31c
Felix Klauke <info@felix-klauke.de>
2019-07-30 20:36:24 +0200

    Properly tag images.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@
 # Login to docker hub
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build --build-arg PAPER_VERSION=$PAPER_VERSION .
+docker build -t felixklauke/paperspigot:$PAPER_VERSION --build-arg PAPER_VERSION=$PAPER_VERSION .
 docker push felixklauke/paperspigot:$PAPER_VERSION
