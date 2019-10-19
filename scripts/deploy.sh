55fc8f33945e1d5c0a2473b78e19dd4815680cf8
fisher <george.kokinis@gmail.com>
2019-10-19 16:39:28 +0100

    Delete deploy.sh
    Not needed for this.

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Login to docker hub
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
-docker build -t felixklauke/paperspigot:$PAPER_VERSION --build-arg PAPER_VERSION=$PAPER_VERSION .
-docker push felixklauke/paperspigot:$PAPER_VERSION
