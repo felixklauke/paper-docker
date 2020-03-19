fe493d9fe399ece76ac1c17e2f62b4e27b4220f1
Felix Klauke <info@felix-klauke.de>
2020-03-19 07:39:30 +0100

    Hotfix java version not used in build args

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@
 # Login to docker hub
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build -t felixklauke/paperspigot:$PAPER_VERSION --build-arg PAPER_VERSION=$PAPER_VERSION .
+docker build -t felixklauke/paperspigot:$PAPER_VERSION --build-arg PAPER_VERSION=$PAPER_VERSION --build-arg JAVA_VERSION=$JAVA_VERSION .
 docker push felixklauke/paperspigot:$PAPER_VERSION
